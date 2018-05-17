401e45f09b48e6baa78c836a82d7f43af5ed866f
Fotis Tsalampounis <ftsalamp@gmail.com>
2018-05-17 16:49:59 +0300

    Change loading image

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,11 +1,11 @@
-import infinityLoaderPath from '../img/infinity-loader.svg';
+import waybackLoaderPath from '../img/wayback-loader.svg';
 import React from 'react';
 
 export default class Loading extends React.Component {
   render () {
     return (
       <div className="loading">
-        <object type="image/svg+xml" data={infinityLoaderPath} />
+        <object type="image/svg+xml" data={waybackLoaderPath} />
       </div>
     );
   }
