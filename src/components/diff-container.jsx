af33b70efb9f8bd7156686d2dd803eaa695a0d0c
Fotis Tsalampounis <ftsalamp@gmail.com>
2018-05-17 20:24:44 +0300

    Support second type of URL

--- src/components/diff-container.jsx
+++ src/components/diff-container.jsx
@@ -19,14 +19,35 @@ export default class DiffContainer extends React.Component {
 
     return (
         <Router>
-        <Route path = '/:diffType' render={({match, history}) =>
-          <div className="diffcontainer-view">
-              {this.exportQueryParams(history.location.search, match.params)}
-        </div>
-          }
-        />
-      </Router>
+            <switch>
+                <Route path = '/diff/:timestampA/:timestampB/:site' render={({location}) =>
+                    <div className="diffcontainer-view">
+                        {this.exportParams(location.pathname)}
+                    </div>
+                }
+                />
+                <Route exact path = '/:diffType' render={({match, history}) =>
+                  <div className="diffcontainer-view">
+                      {this.exportQueryParams(history.location.search, match.params)}
+                </div>
+                }
+                />
+            </switch>
+        </Router>
     );
+  }
+
+  exportParams(path){
+
+      if (/[0-9]{14}\/[0-9]{14}\/.+/.test(path)) {
+
+          let site = path.substring(36);
+          let urlA = 'https://web.archive.org/web/' + path.substring(6,20) + '/' + site;
+          let urlB = 'https://web.archive.org/web/' + path.substring(21,35) + '/' + site;
+
+          return <DiffView page = {{url: site}}
+                           diffType='SIDE_BY_SIDE_RENDERED' a={urlA} b={urlB} />
+      }
   }
 
   exportQueryParams(query, matchP){
