dbe3b99b0faabaf0156804a1396223435e76ebba
Fotis Tsalampounis <ftsalamp@gmail.com>
2019-02-13 16:52:45 +0200

    Implement weightOfUint8Array

--- src/js/utils.js
+++ src/js/utils.js
@@ -40,63 +40,37 @@ export function handleRelativeURL (url) {
   return `${window.location.protocol}//${window.location.hostname}:${window.location.port}${url}`;
 }
 
-export function hammingWeight(l) {
-  var c;
-  for(c = 0; l; c++) {
-    l &= l-1;
-  }
-  return c;
-}
-
-// export function similarity(simhash1, simhash2) {
-//   return hammingWeight((simhash1 & simhash2)) / hammingWeight((simhash1 | simhash2));
+// export function hammingWeight(l) {
+//   var c;
+//   for(c = 0; l; c++) {
+//     l &= l-1;
+//   }
+//   return c;
 // }
 
-/**
- * Hamming distance between ints
- *
- * @param x {number}
- * @param y {number}
- * @returns {number}
- */
-function distanceOfInts (x, y) {
-  return weightOfInt(x ^ y);
-}
-
-/**
- * Hamming distance between Uint8Array
- *
- * @param x {number}
- * @param y {number}
- * @returns {number}
- */
-function distanceOfUint8Array (x, y) {
-  return _.zip(x, y)
-    .map(([i, j]) => weightOfInt(i ^ j))
-    .reduce((acc, w) => acc + w, 0);
-}
-
-/**
- * Hamming Distance
- * https://en.wikipedia.org/wiki/Hamming_distance
- *
- * @param x
- * @param y
- * @returns {number}
- */
-
-export function similarity(x, y) {
-  if (Number.isInteger(x) && Number.isInteger(y)) {
-    return distanceOfInts(x, y);
+export function similarity(simhash1, simhash2) {
+  if (Number.isInteger(simhash1) && Number.isInteger(simhash2)) {
+    return weight((simhash1 & simhash2)) / weight((simhash1 | simhash2));
   }
 
-  if (x instanceof Uint8Array && y instanceof Uint8Array) {
-    return distanceOfUint8Array(x, y);
+  if (simhash1 instanceof Uint8Array && simhash2 instanceof Uint8Array) {
+    let andArray = [];
+    let orArray = [];
+    for (let i = 0; i < simhash1.length; i++) {
+      andArray.push(simhash1[i] & simhash2[i]);
+      orArray.push(simhash1[i] | simhash2[i]);
+    }
+    return weightOfUint8Array(andArray) / weightOfUint8Array(orArray);
   }
-
-  throw new Error(`Unsupported types: ${typeof x} ${typeof y}`);
 }
 
+/**
+ * Hamming weight of number
+ *
+ * @private
+ * @param x {number}
+ * @returns {number}
+ */
 function weightOfInt (x) {
   let sum = 0;
   while (x !== 0) {
@@ -104,6 +78,36 @@ function weightOfInt (x) {
     x &= x - 1;
   }
   return sum;
+}
+
+/**
+ * Hamming weight of uint8array
+ *
+ * @private
+ * @param x
+ * @returns {number}
+ */
+function weightOfUint8Array (x) {
+  return x.reduce((acc, i) => weightOfInt(i) + acc, 0);
+}
+
+/**
+ * Hamming Weight
+ * https://en.wikipedia.org/wiki/Hamming_weight
+ *
+ * @param x
+ * @returns {number}
+ */
+export function weight (x) {
+  if (Number.isInteger(x)) {
+    return weightOfInt(x);
+  }
+
+  if (x instanceof Uint8Array) {
+    return weightOfUint8Array(x);
+  }
+
+  throw new Error('Unsupported type');
 }
 
 
