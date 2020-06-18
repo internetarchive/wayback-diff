3a3c89a70400cf998b21dc390d1248fc15350d6f
Vangelis Banos <vangelis@archive.org>
2020-06-18 22:32:24 +0000

    Remove redundant <div> from footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../css/diff-container.css';
 
-
 /**
  * Display a footer explaining the colors showing in the diffs
  *
@@ -9,16 +8,12 @@ import '../css/diff-container.css';
  * @extends {React.Component}
  */
 export default class DiffFooter extends React.Component {
-
-
   render () {
-    // console.log('diff-Footer render');
     return (
-      <div>
-        <p id={'diff-footer'}><yellow-diff-footer>Yellow</yellow-diff-footer> indicates content deletion. <blue-diff-footer>Blue</blue-diff-footer> indicates content addition
-        </p>
-      </div>
+      <p id={'diff-footer'}><yellow-diff-footer>Yellow</yellow-diff-footer>
+        indicates content deletion. <blue-diff-footer>Blue</blue-diff-footer>
+        indicates content addition.
+      </p>
     );
   }
-
 }
