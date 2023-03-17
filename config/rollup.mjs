9208e6c11a8d6d611a5a5e20b85f45b92b7b0272
Vangelis Banos <vangelis@archive.org>
2023-03-17 13:36:31 +0000

    Update all rollup plugins and related conf

--- config/rollup.mjs
+++ config/rollup.mjs
@@ -1,11 +1,10 @@
 // Rollup plugins.
 import { babel } from '@rollup/plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import globals from 'rollup-plugin-node-globals';
+import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
-import replace from 'rollup-plugin-replace';
-import resolve from 'rollup-plugin-node-resolve';
-import url from 'rollup-plugin-url';
+import replace from '@rollup/plugin-replace';
+import resolve from '@rollup/plugin-node-resolve';
+import url from '@rollup/plugin-url';
 
 import cssnano from 'cssnano';
 import terser from '@rollup/plugin-terser';
@@ -32,7 +31,8 @@ export default {
       plugins: [ '@babel/plugin-proposal-export-default-from' ]
     }),
     replace({ 'process.env.NODE_ENV': JSON.stringify('development'),
-      'process.env': JSON.stringify(process.env)}),
+      'process.env': JSON.stringify(process.env),
+      'preventAssignment': true}),
     resolve({
       browser: true,
       main: true
@@ -41,13 +41,7 @@ export default {
       include: [
         'node_modules/**'
       ],
-      exclude: 'node_modules/process-es6/**',
-      namedExports: {
-        'node_modules/react/index.js': ['PureComponent', 'Component'],
-        'node_modules/simple-xpath-position/index.js': ['fromNode'],
-        'node_modules/react-is/index.js': ['isValidElementType']
-      }
-    }),
-    globals()
+      exclude: 'node_modules/process-es6/**'
+    })
   ]
 };
