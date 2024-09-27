bd65e163a894d8064f274c5bdd0ecdc3209b0324
Vangelis Banos <vangelis@archive.org>
2024-09-27 18:53:15 +0000

    Import only what we use from d3 in the sunburst component

--- src/components/sunburst/d3-sunburst.jsx
+++ src/components/sunburst/d3-sunburst.jsx
@@ -1,8 +1,10 @@
 // Display a d3 Sunburst diagram
-import * as d3 from 'd3';
 import PropTypes from 'prop-types';
 import React from 'react';
 import round from 'lodash/round';
+import { select } from 'd3-selection';
+import { hierarchy, partition } from 'd3-hierarchy';
+import { arc } from 'd3-shape';
 import { getSize } from './sunburst-container-utils.js';
 
 function getDistance (capture) {
@@ -34,21 +36,20 @@ export default class D3Sunburst extends React.Component {
     const radius = Math.min(width, height) / 2;
 
     // Create primary <g> element
-    const g = d3.select(this.chartRef.current)
+    const g = select(this.chartRef.current)
       .attr('width', width)
       .attr('height', height)
       .append('g')
       .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
     // Data strucure
-    const partition = d3.partition().size([2 * Math.PI, radius]);
+    const structure = partition().size([2 * Math.PI, radius]);
 
     // Find data root
-    const root = d3.hierarchy(simhashData)
-      .sum(function (d) { return 5; });
+    const root = hierarchy(simhashData).sum(function (d) { return 5; });
     // Size arcs
-    partition(root);
-    const arc = d3.arc()
+    structure(root);
+    const myarc = arc()
       .startAngle(function (d) { return d.x0; })
       .endAngle(function (d) { return d.x1; })
       .innerRadius(function (d) { return d.y0; })
@@ -60,15 +61,15 @@ export default class D3Sunburst extends React.Component {
       .data(root.descendants())
       .enter().append('path')
       .attr('display', function (d) { return d.depth ? null : 'none'; })
-      .attr('d', arc)
+      .attr('d', myarc)
       .style('stroke', '#fff')
       .style('fill', function (d) { return d.data.clr; })
       .on('mouseover', function (e, d) {
-        d3.select(e.currentTarget).style('cursor', 'pointer').style('stroke', 'black');
+        select(e.currentTarget).style('cursor', 'pointer').style('stroke', 'black');
         component.setState({ hint: getDistance(d.data) });
       })
       .on('mouseleave', function (e, d) {
-        d3.select(e.currentTarget).style('cursor', 'default').style('stroke', '');
+        select(e.currentTarget).style('cursor', 'default').style('stroke', '');
         component.setState({ hint: '' });
       })
       .on('click', function (e, d) {
