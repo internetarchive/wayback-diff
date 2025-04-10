b64969865bc6eba1d43baa2111a71b3f0a8a0dc7
vbanos <vbanos@gmail.com>
2025-04-10 11:58:51 +0200

    Add missing ; in test

--- src/components/__tests__/error-component.test.jsx
+++ src/components/__tests__/error-component.test.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react';
 import ErrorMessage from '../errors';
 
 describe('Error component tests', function () {
