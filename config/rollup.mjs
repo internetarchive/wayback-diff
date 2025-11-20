7d7275f62503bdff2b466b26221146c116dd0f97
vbanos <vbanos@gmail.com>
2025-11-20 19:01:37 +0100

    Replace obsolete react-scripts with rollup

--- config/rollup.mjs
+++ config/rollup.mjs
@@ -1,13 +1,17 @@
-// Rollup plugins.
 import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 import replace from '@rollup/plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
 import url from '@rollup/plugin-url';
-
 import cssnano from 'cssnano';
 import terser from '@rollup/plugin-terser';
+
+// Dev plugins
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
+const isDev = process.env.ROLLUP_WATCH === 'true';
 
 export default {
   input: 'src/index-build.js',
@@ -26,21 +30,28 @@ export default {
       exclude: 'node_modules/process-es6/**'
     }),
     replace({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-      'preventAssignment': true
+      'process.env.NODE_ENV': JSON.stringify(isDev ? 'development' : 'production'),
+      preventAssignment: true
     }),
     url(),
     postcss({
-      extensions: [ '.css' ],
+      extensions: ['.css'],
       plugins: [cssnano()]
     }),
     babel({
       babelrc: false,
       exclude: 'node_modules/**',
       babelHelpers: 'bundled',
-      presets: [ '@babel/preset-env', '@babel/preset-react' ],
-      plugins: [ '@babel/plugin-proposal-export-default-from' ]
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      plugins: ['@babel/plugin-proposal-export-default-from']
     }),
-    terser()
-  ]
+    !isDev && terser(), // only minify in production
+    isDev && serve({
+      open: true,
+      contentBase: ['build', 'public'],
+      host: '0.0.0.0',
+      port: 5000
+    }),
+    isDev && livereload({ watch: 'build' })
+  ].filter(Boolean)
 };
