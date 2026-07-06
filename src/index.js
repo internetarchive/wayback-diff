4fb9a09825a368b879e91d6a583172be577b5059
vbanos <vbanos@gmail.com>
2026-07-06 14:07:41 +0200

    feat: migrate react-router-dom to v6 API
    - Switch -> Routes, Route render/component -> Route element
    - Replace regex paths with wildcard /diff/* and /diffgraph/* routes
    - DiffRoute and DiffGraphRoute components parse the wildcard segment
      using the original regex patterns

--- src/index.js
+++ src/index.js
@@ -2,52 +2,58 @@
 import React, { StrictMode } from 'react';
 /* eslint-enable no-unused-vars */
 
-import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import DiffContainer from './components/diff-container.jsx';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useParams, useLocation } from 'react-router-dom';
 import SunburstContainer from './components/sunburst/sunburst-container.jsx';
 
 import conf from './conf.json';
-const root = createRoot(document.getElementById('wayback-diff'));
 
-// /hello endpoint is to test that React routing works.
+function DiffRoute () {
+  const { '*': rest } = useParams();
+  const { search } = useLocation();
+
+  const m2 = rest.match(/^([0-9]{14})\/([0-9]{14})\/(.+)$/);
+  if (m2) {
+    return <DiffContainer url={m2[3] + search} timestampA={m2[1]} timestampB={m2[2]} loader={null} conf={conf} />;
+  }
+
+  const mA = rest.match(/^([0-9]{14})\/\/(.+)$/);
+  if (mA) {
+    return <DiffContainer url={mA[2] + search} timestampA={mA[1]} loader={null} conf={conf} />;
+  }
+
+  const mB = rest.match(/^\/([0-9]{14})\/(.+)$/);
+  if (mB) {
+    return <DiffContainer url={mB[2] + search} timestampB={mB[1]} loader={null} conf={conf} />;
+  }
+
+  const mNoTs = rest.match(/^\/\/(.+)$/);
+  if (mNoTs) {
+    return <DiffContainer url={mNoTs[1] + search} conf={conf} noTimestamps={true} loader={null} />;
+  }
+
+  return <DiffContainer url={rest + search} loader={null} conf={conf} />;
+}
+
+function DiffGraphRoute () {
+  const { '*': rest } = useParams();
+  const { search } = useLocation();
+  const m = rest.match(/^([0-9]{14})\/(.+)$/);
+  if (!m) return null;
+  return <SunburstContainer url={m[2] + search} timestamp={m[1]} loader={null} conf={conf} />;
+}
+
+const root = createRoot(document.getElementById('wayback-diff'));
 
 root.render(
   <StrictMode>
     <Router>
-      <Switch>
-        <Route path="/hello" component={() => <div>Hello, this is static text!</div>} />
-
-        <Route path='/diff/([0-9]{14})/([0-9]{14})/(.+)' render={({ match, location }) =>
-          <DiffContainer url={match.params[2] + location.search} timestampA={match.params[0]}
-            loader={null}
-            timestampB={match.params[1]} conf={conf} />
-        } />
-        <Route path='/diff/([0-9]{14})//(.+)' render={({ match, location }) =>
-          <DiffContainer url={match.params[1] + location.search} timestampA={match.params[0]}
-            loader={null}
-            conf={conf} />
-        } />
-        <Route path='/diff//([0-9]{14})/(.+)' render={({ match, location }) =>
-          <DiffContainer url={match.params[1] + location.search} timestampB={match.params[0]}
-            loader={null}
-            conf={conf} />
-        } />
-
-        <Route path='/diff///(.+)' render={({ match, location }) =>
-          <DiffContainer url={match.params[0] + location.search} conf={conf} noTimestamps={true}
-            loader={null}/>
-        } />
-        <Route path='/diff/(.+)' render={({ match, location }) =>
-          <DiffContainer url={match.params[0] + location.search}
-            loader={null} conf={conf}/>}
-        />
-        <Route path='/diffgraph/([0-9]{14})/(.+)' render={({ match, location }) =>
-          <SunburstContainer url={match.params[1] + location.search} timestamp={match.params[0]}
-            loader={null}
-            conf={conf} />}
-        />
-      </Switch>
+      <Routes>
+        <Route path="/hello" element={<div>Hello, this is static text!</div>} />
+        <Route path="/diff/*" element={<DiffRoute />} />
+        <Route path="/diffgraph/*" element={<DiffGraphRoute />} />
+      </Routes>
     </Router>
-  </StrictMode>);
+  </StrictMode>
+);
